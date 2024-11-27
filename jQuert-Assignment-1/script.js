91d13b2151072714b676fbff5b147eaf8a464d64
Aswan R <aswan.r@coddletech.com>
2024-11-27 18:07:57 +0530

    first commit

--- jQuert-Assignment-1/script.js
+++ jQuert-Assignment-1/script.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-    // 1)A page has a series of product cards, each containing a header, small description, and a 
-    // "Delete" button. Clicking the button should remove the card from the DOM.
-    $('.delete').click(function () {
-        $(this).parent().remove();
-    });
+// 1)A page has a series of product cards, each containing a header, small description, and a 
+// "Delete" button. Clicking the button should remove the card from the DOM.
+$('.delete').click(function(){
+     $(this).parent().remove(); 
+});
 
-    // 2)On the same set of cards, users can hover over a product card to see additional details.
-    //  The details should hide again when the user moves the mouse away.
+// 2)On the same set of cards, users can hover over a product card to see additional details.
+//  The details should hide again when the user moves the mouse away.
     $('img').hide();
-    $(".cards").mouseenter(function () {
+    $(".cards").hover(function () {
         $(this).find('h2').hide();
         $(this).find('img').show(1000);
-    })
-    $(".cards").mouseleave(function () {
-        $(this).find('h2').show(500);
-        $(this).find('img').hide();
-    })
+        }, function () {
+            $(this).find('h2').show(500);
+            $(this).find('img').hide();
+        }
+    );    
 })
